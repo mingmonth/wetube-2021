05df401772d2b7adcd87f415a75023114b020032
ys9922 <ys9922@naver.com>
2021-05-04 20:34:12 +0900

    nanoexpress framework 적용

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+import nanoexpress from "nanoexpress";
+
+const app = nanoexpress();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  return res.send({ status: "ok" });
+});
+
+app.listen(PORT);
