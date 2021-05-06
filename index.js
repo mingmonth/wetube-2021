bd990b9e8972905133b84aba550fbbf7c65b4478
ys9922 <ys9922@naver.com>
2021-05-06 09:59:34 +0900

    #3.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import nanoexpress from "nanoexpress";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = nanoexpress();
 
@@ -14,14 +16,17 @@ const handleProfile = (req, res) => {
   return res.send("handle profile2");
 };
 
-const middleware = (req, res, next) => {
-  return res.send("not happening!!!");
-};
+// const middleware = (req, res, next) => {
+//   return res.send("not happening!!!");
+// };
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
 app.get("/", handleHome);
-app.get("/profile", middleware, handleProfile);
+app.get("/profile", handleProfile);
 
 app.listen(PORT);
