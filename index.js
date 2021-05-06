80b066b49099e9284a09ec42783b2457565f6d81
ys9922 <ys9922@naver.com>
2021-05-06 09:46:53 +0900

    middleware 로 연결끊기

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import nanoexpress from "nanoexpress";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = nanoexpress();
 
@@ -13,13 +15,13 @@ const handleProfile = (req, res) => {
 };
 
 const middleware = (req, res, next) => {
-  console.log("middleware");
-  next();
+  return res.send("not happening!!!");
 };
 
-app.use(middleware);
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.get("/profile", middleware, handleProfile);
 
 app.listen(PORT);
