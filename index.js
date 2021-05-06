3bc5866da4925bd0abc8d913e31e74e8a48b6c7f
ys9922 <ys9922@naver.com>
2021-05-06 09:35:37 +0900

    #3.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const app = nanoexpress();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+const handleHome = (req, res) => {
   return res.send({ status: "ok" });
-});
+};
+
+const handleProfile = (req, res) => {
+  return res.send("handle profile2");
+};
+
+const middleware = (req, res, next) => {
+  console.log("middleware");
+  next();
+};
+
+app.use(middleware);
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT);
