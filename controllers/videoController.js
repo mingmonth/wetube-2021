07319df91d3db633335a25d88aa3431c6e37fe6c
ys9922 <ys9922@naver.com>
2021-05-14 18:51:32 +0900

    #3.21 Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => {
   // const searchingBy = req.query.term;
   const {
@@ -7,13 +10,13 @@ export const search = (req, res) => {
   return res.render("search", { pageTitle: "Search", searchingBy });
 };
 
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
+// export const videos = (req, res) =>
+//   res.render("videos", { pageTitle: "Videos" });
 export const uploadVideo = (req, res) =>
   res.render("uploadVideo", { pageTitle: "Upload Video" });
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
---
+++ db.js
@@ -0,0 +1,51 @@
+// fake data
+export const videos = [
+  {
+    id: 324393,
+    title: "Video 1",
+    description: "This is a Video 1 description",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "yskim",
+      email: "yskim@abc.com",
+    },
+  },
+  {
+    id: 324378,
+    title: "Video 2",
+    description: "This is a Video 2 description",
+    views: 19,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "yskim",
+      email: "yskim@abc.com",
+    },
+  },
+  {
+    id: 234373,
+    title: "Video 3",
+    description: "This is a Video 3 description",
+    views: 11,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "yskim",
+      email: "yskim@abc.com",
+    },
+  },
+  {
+    id: 176393,
+    title: "Video 4",
+    description: "This is a Video 4 description",
+    views: 27,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "yskim",
+      email: "yskim@abc.com",
+    },
+  },
+];
