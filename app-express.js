fdbbcbd86f6d76376b1878f653e946eff43c972d
ys9922 <ys9922@naver.com>
2021-05-18 16:41:14 +0900

    #5.3 ES6 with Webpack

--- app-express.js
+++ app-express.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 import "../scss/styles.scss";
+
+const something = async () => {
+  await console.log("something");
+};
